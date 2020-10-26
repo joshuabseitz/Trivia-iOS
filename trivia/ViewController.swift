9d11561a6e1ee9385c1c3f7cdaa83589a36ff556
Joshua Seitz <jseitz2@luc.edu>
2020-10-26 15:53:01 -0500

    single view setup

---
+++ trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trivia
+//
+//  Created by Josh on 10/26/20.
+//  Copyright © 2020 Joshua Seitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
