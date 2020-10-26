a71fcf40a957475a73a13c30b9be4f40a6b1f0d8
Joshua Seitz <jseitz2@luc.edu>
2020-10-26 17:18:37 -0500

    🗄️  PersistenceController created - save a new highschore, delete a highscore, and fetch all highscores

---
+++ trivia/Model/PersistenceController.swift
@@ -0,0 +1,36 @@
+//
+//  PersistenceController.swift
+//  trivia
+//
+//  Created by Josh on 10/26/20.
+//  Copyright © 2020 Joshua Seitz. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+struct PersistenceController {
+	
+	static var container: NSPersistentContainer!
+	
+	static func save(_ highscore: Highscore) {
+		
+		do {
+			try container.viewContext.save()
+		} catch {
+			print(error)
+		}
+	}
+	
+	static func getAllHighscores() -> [Highscore] {
+		let highscoreFetchRequest: NSFetchRequest<Highscore> = Highscore.fetchRequest()
+		let highscoreResult = try! container.viewContext.fetch(highscoreFetchRequest)
+		return highscoreResult
+	}
+	
+	static func delete(_ highscore: Highscore){
+		container.viewContext.delete(highscore)
+		try! container.viewContext.save()
+	}
+	
+}
