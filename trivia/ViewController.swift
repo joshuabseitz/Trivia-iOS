aadb57f9eccd12d951b473913b54842a1f09bd15
Joshua Seitz <jseitz2@luc.edu>
2020-10-28 11:34:01 -0500

    🤠 The JSON Chronicles: We're grabbing data locally now ((:

--- trivia/ViewController.swift
+++ trivia/ViewController.swift
@@ -22,30 +22,74 @@ class ViewController: UIViewController {
 	var questionBank = [Questions]()
 	
 //	MARK: - View Lifecycles
+
+//	override func viewWillAppear(_ animated: Bool) {
+//		super.viewWillAppear(animated) // No need for semicolon
+//		getData(url: "https://jsonkeeper.com/b/CVHP")
+//	}
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		getData(url: "https://jsonkeeper.com/b/CVHP")
 		nextButton.isHidden = true
+		let data = readLocalFile(forName: "data")
+		
+		let questions = try! JSONDecoder().decode([Questions].self, from: data!)
+		
+		print(questions)
 	}
 	
+// 	MARK: - Trivia Logic
+	
+//	func refreshQuestion() {
+//		let randomQuestion = questionBank.randomElement()
+//		let question = randomQuestion?.question
+//		let correct = randomQuestion?.correct
+//
+//		var choices = randomQuestion?.incorrect
+//		choices!.append(correct!)
+//		choices!.shuffle()
+//
+//		let buttons: [ChoiceButton] = [choice1, choice2, choice3, choice4,]
+//
+//		var count = 0
+//		for button in buttons {
+//			button.titleLabel!.text = choices![count]
+//			count += 1
+//		}
+//	}
 	
 //	MARK: - JSON Functions
 	
-	fileprivate func getData(url: String) {
-
-		let url = URL(string: url)
-		URLSession.shared.dataTask(with: url!) {(data, response, error) in
-			let questions = try! JSONDecoder().decode([Questions].self, from: data!)
-			self.saveQuestions(questions)
-		}.resume()
+	private func readLocalFile(forName name: String) -> Data? {
+		do {
+			if let bundlePath = Bundle.main.path(forResource: name,
+												 ofType: "json"),
+				let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+				return jsonData
+			}
+		} catch {
+			print(error)
+		}
+		
+		return nil
 	}
 	
-	func saveQuestions(_ questions: [Questions]) {
-		questionBank = questions
-	}
+	//	fileprivate func getData(url: String) {
+	//
+	//		let url = URL(string: url)
+	//		URLSession.shared.dataTask(with: url!) {(data, response, error) in
+	//			let questions = try! JSONDecoder().decode([Questions].self, from: data!)
+	//			self.saveQuestions(questions)
+	//		}.resume()
+	//	}
+	//
+	//	func saveQuestions(_ questions: [Questions]) {
+	//		questionBank = questions
+	//	}
+	//
+	//	func jsonNotFound() {
+	//		print("JSON not found")
+	//	}
 	
-	func jsonNotFound() {
-		print("JSON not found")
-	}
+	
 }
