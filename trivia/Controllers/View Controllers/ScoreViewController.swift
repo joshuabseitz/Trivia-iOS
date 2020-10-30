78b137923cb8045dca57b471dfe5d995e04e8649
Joshua Seitz <jseitz2@luc.edu>
2020-10-30 11:22:49 -0500

    Custom xib for tableview setup

--- trivia/Controllers/View Controllers/ScoreViewController.swift
+++ trivia/Controllers/View Controllers/ScoreViewController.swift
@@ -8,14 +8,49 @@
 
 import UIKit
 
-class ScoreViewController: UIViewController {
+//MARK: - IBOutlets
 
-    override func viewDidLoad() {
+
+
+class ScoreViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+	
+	let data = ["One","Two","Three","Four","Five",]
+	
+	func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
+    }
+	
+	func tableView(_ tableView: UITableView,
+				   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		if let cell = scoreTable.dequeueReusableCell(withIdentifier: "ScoreTableViewCell") as? ScoreTableViewCell {
+			return cell
+		}
+		
+		return UITableViewCell()
+	}
+	
+
+	@IBOutlet weak var scoreTable: UITableView!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
 		navigationController?.setNavigationBarHidden(true, animated: false)
+		
+		self.scoreTable.dataSource = self
+		self.scoreTable.delegate = self
+		self.registerTableViewCells()
     }
 	
 	override var preferredStatusBarStyle: UIStatusBarStyle {
 		return .lightContent
 	}
+	
+	private func registerTableViewCells() {
+		let scoreLabelCell = UINib(nibName: "ScoreTableViewCell",
+								  bundle: nil)
+		self.scoreTable.register(scoreLabelCell,
+								forCellReuseIdentifier: "ScoreTableViewCell")
+	}
+	
 }
---
+++ trivia/Views/ScoreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScoreTableViewCell.swift
+//  trivia
+//
+//  Created by Josh on 10/30/20.
+//  Copyright © 2020 Joshua Seitz. All rights reserved.
+//
+
+import UIKit
+
+class ScoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
